076aa6dceb2058220291eae80d638e15ac0a101d
Harish Chandra Yadav <harish-yadav@newgen.co.in>
2019-10-18 12:37:21 +0530

    First POM Project

---
+++ src/main/java/test/hbm/annotation/HibernateAnnotation/App.java
@@ -0,0 +1,32 @@
+package test.hbm.annotation.HibernateAnnotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+	}
+
+	void testMethod() {
+		method1();
+		method2();
+		method3();
+		method4();
+	}
+	void method1() {
+
+	}
+	void method2() {
+
+	}
+	void method3() {
+
+	}
+	void method4() {
+
+	}
+}
